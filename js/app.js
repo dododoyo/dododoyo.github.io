b04e07cd8163274a82400ee7567b0d23a6a997f6
dododoyo <dolphinmulugeta88@gmail.com>
2024-09-25 15:31:14 +0300

    🆕 Updated Portfolio

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
 
-
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
@@ -21,6 +20,22 @@ navBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
+function calculateAge(birthDate) {
+  const today = new Date();
+  const birth = new Date(birthDate);
+  let age = today.getFullYear() - birth.getFullYear();
+  const monthDifference = today.getMonth() - birth.getMonth();
+  if (
+    monthDifference < 0 ||
+    (monthDifference === 0 && today.getDate() < birth.getDate())
+  ) {
+    age--;
+  }
+  return age;
+}
+
+// set age
+document.getElementById("age").textContent = calculateAge("2002-01-20");
 
 // set year
 date.innerHTML = new Date().getFullYear();
